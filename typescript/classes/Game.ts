142d3039b58a8871e915faac06c0c5d2225817e3
Nuur Yusuf <112573333+nyusufdot88@users.noreply.github.com>
2024-09-07 12:30:45 +0200

    Uppdated GAmed class with createPlayer and GameMode methods

--- typescript/classes/Game.ts
+++ typescript/classes/Game.ts
@@ -23,5 +23,24 @@ export default class Game {
     }
   }
 
+  chooseGameMode(): void{
+    console.clear()
+    console.log('Välje spelläge mata in (1) eller (2)\n');
+    const mode = prompt('1- Human vs Human\n2- Human vs AI ....');
+    this.isVsAI = mode === '2';
+  }
+  
+  createPlayers(): void {
+    console.clear();
+    console.log('Connect-FOUR\n');
+    this.playerX = new Player(prompt('Spelare X:s namn: '), 'X', this.board);
+
+    if (this.isVsAI) {
+      this.playerO = new Player('Computer', 'O', this.board); // AI player
+    } else {
+      this.playerO = new Player(prompt('Spelare O:s namn: '), 'O', this.board)
+    }
+  }
+
 
 }
